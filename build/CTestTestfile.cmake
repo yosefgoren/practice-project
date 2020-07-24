6fb320a2de8300b0c8c6f6dbce53fba2b25b5405
kostya <try1.zipzap@gmail.com>
2020-07-24 13:10:44 +0300

    ignored

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/mtm/practice_project
-# Build directory: C:/mtm/practice_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
