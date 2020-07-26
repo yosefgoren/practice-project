d6e73486e3e5df9120524ac2cc9bfe6e74f6fcda
kostya <try1.zipzap@gmail.com>
2020-07-26 14:48:39 +0300

    first compile

--- source/ParkingLot.cpp
+++ source/ParkingLot.cpp
@@ -1,24 +1,24 @@
 #include "../include/ParkingLot.h"
 
-using namespace MtmParkingLot;
+// using namespace MtmParkingLot;
 
-ParkingLot::ParkingLot(unsigned int parkingBlockSizes[]) :
-    bike(parkingBlockSizes[0]),
-    car(parkingBlockSizes[1]),
-    handicapped(parkingBlockSizes[2])
-{}
+// ParkingLot::ParkingLot(unsigned int parkingBlockSizes[]) :
+//     bike(parkingBlockSizes[0]),
+//     car(parkingBlockSizes[1]),
+//     handicapped(parkingBlockSizes[2])
+// {}
 
-//default d'tor works ?
-// ParkingLot::~ParkingLot()
+// //default d'tor works ?
+// // ParkingLot::~ParkingLot()
+// // {
+// //     bike.~Block();
+// //     car.~Block();
+// //     handicapped.~Block();
+// // }
+
+// ParkingResult ParkingLot::enterParking(VehicleType vehicleType, LicensePlate licensePlate, Time entranceTime)
 // {
-//     bike.~Block();
-//     car.~Block();
-//     handicapped.~Block();
-// }
-
-ParkingResult ParkingLot::enterParking(VehicleType vehicleType, LicensePlate licensePlate, Time entranceTime)
-{
     
-}
+// }
 
 
